338da564bbc09cd898cd83865c788422b8d963df
ualanyal <ualanyal@gmail.com>
2023-12-03 14:39:45 +0100

    - selecting, removing  & dragging points - creating segments & fine tuning

---
+++ js/math/utils.js
@@ -0,0 +1,16 @@
+function getNearestPoint(loc, points, threshhold = Number.MAX_SAFE_INTEGER) {
+    let minDist = Number.MAX_SAFE_INTEGER;
+    let nearest = null;
+    for (const point of points) {
+        const dist = distance(point, loc);
+        if (dist < minDist && dist < threshhold) {
+            minDist = dist;
+            nearest = point;
+        }    
+    }
+    return nearest;
+}
+
+function distance(p1, p2) {
+    return Math.hypot(p1.x - p2.x, p1.y - p2.y);
+}
